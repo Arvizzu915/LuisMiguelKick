0dde22a614e5bd00379bfa826e1ba4c5302d69ec
alifer-bombgirl <alicia.alvarez@uniat.edu.mx>
2025-11-24 08:13:55 -0600

    spawner close to finish

--- Assets/Scripts/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -10,19 +10,37 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] List<GameObject> enemiesPickPool = new();
     public List<GameObject> currentEnemies = new();
     public List<GameObject> enemiesPooling = new();
-    private int enemiesPoolIndex = 0;
-    
 
+    [SerializeField] private float mapLength = 10;
+
+    private void Start()
+    {
+        List<int> allEnemiesIndexes = new()
+        {
+            0
+        };
+
+        SetEnemiesPool(allEnemiesIndexes);
+
+        InvokeRepeating(nameof(SpawnEnemy), 0f, 1f);
+    }
 
     private void SpawnEnemy()
     {
+        if (currentEnemiesInScene >= maxEnemiesInScene) return;
+
         currentEnemiesInScene++;
 
         int enemiesPick = Random.Range(0, enemiesPickPool.Count);
 
-        enemiesPooling[enemiesPoolIndex] = enemiesPickPool[enemiesPick];
+        GameObject enemyPooled = GetEnemyPooling();
 
-        enemiesPoolIndex++;
+        enemyPooled = enemiesPickPool[enemiesPick];
+        enemyPooled.SetActive(true);
+
+        Vector3 mapPos = GetRandomPositionInMap();
+
+        enemyPooled.transform.position = mapPos;
     }
 
     public void SetEnemiesPool(List<int> enemiesIndex)
@@ -33,5 +51,36 @@ public class EnemySpawner : MonoBehaviour
         {
             enemiesPickPool.Add(enemies[enemiesIndex[i]]);
         }
+    }
+
+    public GameObject GetEnemyPooling()
+    {
+        GameObject enemyToUse = null;
+
+        for (int i = 0; i < enemiesPooling.Count; i++)
+        {
+            if (!enemiesPooling[i].activeSelf)
+            {
+                enemyToUse = enemiesPooling[i];
+                break;
+            }
+        }
+
+        if (enemyToUse == null)
+        {
+            GameObject newEnemy = Instantiate(enemiesPickPool[0]);
+            newEnemy.SetActive(false);
+            enemiesPooling.Add(newEnemy);
+            enemyToUse = newEnemy;
+        }
+
+        return enemyToUse;
+    }
+
+    public Vector3 GetRandomPositionInMap()
+    {
+        Vector3 randomPos = new(Random.Range(-mapLength, mapLength), 0f, Random.Range(-mapLength, mapLength));
+
+        return randomPos;
     }
 }
